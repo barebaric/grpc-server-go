9e3272f46a89b773638eb9a65bdd9f893ee877ff
Samuel Abels <knipknap@gmail.com>
2020-07-19 09:47:58 +0000

    make globally installed golang modules available during protobuf compilation

--- cmd/compile.sh
+++ cmd/compile.sh
@@ -2,6 +2,6 @@
 
 cd ../proto
 ls -1 *.proto 2>/dev/null | while read FILENAME; do
-    protoc -I. "$FILENAME" --go_out=plugins=grpc:.
+    protoc -I /usr/local/include/ -I. "$FILENAME" --go_out=plugins=grpc:.
 done
 cd -
